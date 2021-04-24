5cb4743748bf4bf108edd62063360ca5df0f0e67
Benjamin Worthington <benworthin@icloud.com>
2021-04-24 18:09:11 -0500

    Implemented given BinarySearch class

---
+++ BinaryTree.cpp
@@ -0,0 +1,110 @@
+//
+// Created by Ben Worthington on 4/24/21.
+//
+
+#include "BinaryTree.h"
+
+BinaryTree::BinaryTree()
+{
+    Root = NULL;
+}
+
+//-----------------------------------------------------------
+// Destructor function.
+//-----------------------------------------------------------
+BinaryTree::~BinaryTree()
+{
+}
+
+//-----------------------------------------------------------
+// Search helper function.
+//-----------------------------------------------------------
+bool BinaryTree::SearchHelper(int Value, Node * Tree)
+{
+    // Data value not found
+    if (Tree == NULL)
+        return false;
+
+        // Data value found
+    else if (Tree->Value == Value)
+        return true;
+
+        // Recursively search for data value
+    else if (Tree->Value > Value)
+        return (SearchHelper(Value, Tree->Left));
+    else if (Tree->Value < Value)
+        return (SearchHelper(Value, Tree->Right));
+}
+
+//-----------------------------------------------------------
+// Search for data in the binary tree.
+//-----------------------------------------------------------
+bool BinaryTree::Search(int Value)
+{
+    // Call tree searching function
+    return (SearchHelper(Value, Root));
+}
+
+//-----------------------------------------------------------
+// Insert helper function.
+//-----------------------------------------------------------
+bool BinaryTree::InsertHelper(int Value, Node * &Tree)
+{
+    // Insert data into the tree
+    if (Tree == NULL)
+    {
+        Tree = new Node;
+        Tree->Value = Value;
+        Tree->Left = NULL;
+        Tree->Right = NULL;
+        return true;
+    }
+
+        // Data value already inserted
+    else if (Tree->Value == Value)
+        return false;
+
+        // Recursively search for insertion position
+    else if (Tree->Value > Value)
+        return (InsertHelper(Value, Tree->Left));
+    else if (Tree->Value < Value)
+        return (InsertHelper(Value, Tree->Right));
+}
+
+//-----------------------------------------------------------
+// Insert data into the binary tree.
+//-----------------------------------------------------------
+bool BinaryTree::Insert(int Value)
+{
+    // Call tree insertion function
+    return (InsertHelper(Value, Root));
+}
+
+//-----------------------------------------------------------
+// Print helper function.
+//-----------------------------------------------------------
+void BinaryTree::PrintHelper(Node * Tree)
+{
+    // Check terminating condition
+    if (Tree != NULL)
+    {
+        // Print left subtree
+        PrintHelper(Tree->Left);
+
+        // Print node value
+        cout << " " << Tree->Value << " ";
+
+        // Print right subtree
+        PrintHelper(Tree->Right);
+    }
+}
+
+//-----------------------------------------------------------
+// Print all records in the binary tree.
+//-----------------------------------------------------------
+void BinaryTree::Print()
+{
+    // Call tree printing function
+    PrintHelper(Root);
+    cout << endl;
+}
---
+++ BinaryTree.h
@@ -0,0 +1,46 @@
+//
+// Created by Ben Worthington on 4/24/21.
+//
+
+#ifndef PF2_LAB13_BINARYTREE_H
+#define PF2_LAB13_BINARYTREE_H
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+// Data node definition
+class Node
+{
+public:
+    int Value;
+    Node *Left;
+    Node *Right;
+};
+
+class BinaryTree {
+public:
+    // Constructor functions
+    BinaryTree();
+    ~BinaryTree();
+
+    // General binary tree operations
+    bool Search(int Value);
+    bool Insert(int Value);
+    bool Delete(int Value);
+    void Print();
+
+private:
+    // Helper functions
+    bool SearchHelper(int Value, Node * Tree);
+    bool InsertHelper(int Value, Node * &Tree);
+    bool DeleteNode(Node * &Tree);
+    bool DeleteHelper(int Value, Node * &Tree);
+    void PrintHelper(Node * Tree);
+
+    // Tree pointer
+    Node *Root;
+};
+
+
+#endif //PF2_LAB13_BINARYTREE_H
